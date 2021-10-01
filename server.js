fa59340488750bab1ae2d886ec656a6123e26c6a
Matthew Slanaker <matt.slanaker@gmail.com>
2021-10-01 17:02:28 -0400

    creating db and connection

--- server.js
+++ server.js
@@ -1,33 +1,37 @@
-const express = require('express');
-const path = require('path')
-const session = require('express-session');
-const routes = require('./controllers');
-
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
-
+const express = require("express");
+const path = require("path");
+const controller = require("./controllers");
+const exphbs = require("express-handlebars");
+const sequelize = require("./config/connection");
+const session = require("express-session");
+const SequlizeStore = require("connect-session-sequelize")(session.Store);
 
 const sess = {
-  secret: 'Super secret secret',
+  secret: "super secret secret",
   cookie: {},
   resave: false,
   saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
+  store: new SequlizeStore({
+    db: sequelize,
+  }),
 };
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(session(sess));
 
-app.use(routes);
+
+app.use("/", controller);
+
+
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
 });
